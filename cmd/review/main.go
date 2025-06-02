c6f0be948ec933a03982be6fd193190c580fee48
Takao Shibata <chise.alter.pasta@gmail.com>
2025-06-02 22:49:57 +0900

    initial implementation

---
+++ cmd/review/main.go
@@ -0,0 +1,53 @@
+// Review the design documents stored in the knowledge base according to the requirements specified in the given URL.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/shibataka000/heimdall/internal/aws/bedrock"
+	wa "github.com/shibataka000/heimdall/internal/checklist/awswellarchitectedframework"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var (
+		agentID string
+	)
+
+	command := &cobra.Command{
+		Use:   "review <url>",
+		Short: "Review the design documents stored in the knowledge base according to the requirements specified in the given URL.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			client, err := bedrock.NewClient(cmd.Context())
+			if err != nil {
+				return err
+			}
+			prompt, err := wa.GeneratePrompt(args[0])
+			if err != nil {
+				return err
+			}
+			response, err := client.InvokeAgent(cmd.Context(), agentID, prompt)
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(response))
+			return nil
+		},
+		SilenceUsage: true,
+	}
+
+	command.Flags().StringVar(&agentID, "agent-id", "", "The unique identifier of the agent to invoke.")
+
+	for _, flag := range []string{"agent-id"} {
+		if err := command.MarkFlagRequired(flag); err != nil {
+			panic(err)
+		}
+	}
+
+	if command.ExecuteContext(context.Background()) != nil {
+		os.Exit(1)
+	}
+}
